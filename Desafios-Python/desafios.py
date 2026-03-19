e77b14952419c3329cb2bdc9c9f82b6b58cbe9b0
Thiagoolivs <thiago.olivs.coelho@gmail.com>
2026-03-19 20:57:12 -0300

    Aula02 - Variáveis e operadores

---
+++ Desafios-Python/desafios.py
@@ -0,0 +1,21 @@
+#DESAFIO 1
+nome = input("Digite seu nome: ")
+
+print(f"Olá {nome}! Seja Bem-vindo!")
+
+#DESAFIO2
+diaNasc = input("Digite o dia do seu nascimento: ")
+mesNasc = input("Digite o mês do seu nascimento: ")
+anoNasc = input("Digite o ano do seu nascimento: ")
+
+dataFormat = f"{diaNasc}/{mesNasc}/{anoNasc}"
+print(f"A data do seu nascimento é: {dataFormat}")
+
+#DESAFIO3
+
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+
+soma = num1 + num2
+
+print(f"A soma dos números é: {soma}")
---
+++ aula02-variaveis-e-operadores/aula02-variaveis.py
@@ -0,0 +1,20 @@
+'''print("Olá mundo")
+
+print(7+4)
+
+#variáveis
+
+nome = ("Thiago")
+idade = 18
+peso = 71.80
+print(nome, idade, peso)
+
+#input - simulação de forms no cmd
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(idade+1)
+
+'''
